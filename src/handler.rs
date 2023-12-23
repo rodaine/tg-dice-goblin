6c52a3dfb2fe50f2a851483c6ad2c42066d5b5b0
Chris Roche <github@rodaine.com>
2023-12-23 13:55:28 -0800

    Switch to grammers

--- src/handler.rs
+++ src/handler.rs
@@ -1,7 +1,15 @@
-use telegram_bot::*;
-use log::{info, warn};
+use grammers_client::{InputMessage, Update};
+use log::{info, trace, warn};
+use nom::branch::alt;
+use nom::bytes::complete::tag_no_case;
+use nom::character::complete::multispace1;
+use nom::character::streaming::char;
+use nom::combinator::{eof, opt, rest};
+use nom::IResult;
+use nom::sequence::{preceded, tuple};
 
-use crate::rolls::parse;
+use crate::{Result, rolls};
+use crate::rolls::Roll;
 
 const START_MSG: &str = "Let *Dice Goblin* roll for you!
 
@@ -9,90 +17,55 @@ Dice Goblin will roll any-sided rolls and perform simple arithmetic to reach a t
 
 const HELP_MSG: &str = "*COMMANDS*
 
-/start
+/start\\
 _See introductory information about this bot_
 
-/help
+/help\\
 _See this help output_
 
-/roll `[expression]`
-_Roll the rolls and calculate a total (see expression syntax below)_
+/roll `[expression]`\\
+_Rolls and calculate a total (see expression syntax below)_
 
-/r `[expression]`
+/r `[expression]`\\
+_Alias for /roll_
+
+/`[expression]`\\
 _Alias for /roll_
 
 *ROLL EXPRESSION SYNTAX*
 
 Dice rolls are described in the standard `NdS` format, where `N` is the number of rolls and `S` is the number of sides. Each roll is summed together to calculate the overall value.
 
-*Examples:*
-`3d10` - Roll a ten-sided die three times
-`d6` - Roll a single six-sided die (N defaults to 1 if omitted)
-`D2` - flip a coin (The `d` is case-insensitive)
+*Examples:*\\
+`3d10` - Roll a ten-sided die three times\\
+`d6` - Roll a single six-sided die (N defaults to 1 if omitted)\\
+`D2` - flip a coin (The `d` is case-insensitive)\\
 
-Rolls support basic arithmetic using the operators (+, -, \\*, /) as well as parenthesis. Division always truncates (or rounds towards zero), and division by zero always equals zero.
+Rolls support basic arithmetic using the operators (+, -, \\*, /) as well as parenthesis. Division always rounds towards zero, and division by zero always equals zero.
 
-*Examples:*
-`3d10 + 2` - Roll three ten-sided rolls and add two to the result
-`(d6 - 1) * 2` - Roll a six-sided die, subtract one from the roll, and then double the result
-`3 / 2` - Equals 1 (1.5 rounded towards zero)
+*Examples:*\\
+`3d10 + 2` - Roll three ten-sided rolls and add two to the result\\
+`(d6 - 1) * 2` - Roll a six-sided die, subtract one from the roll, and then double the result\\
+`3 / 2` - Equals 1 (1.5 rounded towards zero)\\
 `1 / 0` - Division by zero always equals zero";
 
 const UNKNOWN_MSG: &str = "Unknown command. Use /help to see available commands";
 
-const ROLL_ERR_MSG: &str = "I don't understand this roll. Rolls should look something like `5d20 + 3`. See /help for the full syntax.";
-
-pub async fn handle(api: &Api, update: Update) -> Result<(), Error> {
-    let msg = match update.kind {
-        telegram_bot::update::UpdateKind::Message(m) => m,
-        _ => return Ok(()),
-    };
-
-    let (txt, entities) = match &msg.kind {
-        telegram_bot::MessageKind::Text { data, entities } => (data, entities),
-        _ => return Ok(()),
-    };
-
-    let cmd = match Command::resolve(txt, entities) {
-        Some(c) => c,
-        None => return Ok(()),
-    };
-
-    let res = match cmd {
-        Command::Start => {
-            info!("start on {:?}", msg.chat);
-            let mut req = SendMessage::new(msg.chat, START_MSG);
-            req.parse_mode(ParseMode::Markdown);
-            api.send(req).await
-        }
-
-        Command::Help => {
-            info!("help on {:?}", msg.chat);
-            let mut req = SendMessage::new(msg.chat, HELP_MSG);
-            req.parse_mode(ParseMode::Markdown);
-            api.send(req).await
-        }
-        Command::Roll(offset) => {
-            match parse(&txt[offset..]) {
-                Ok(r) => {
-                    info!("good roll on {:?}: {}", msg.chat, r);
-                    api.send(msg.text_reply(format!("{} = {}", r.value(), r))).await
-                }
-                Err(_) => {
-                    warn!("bad roll on {:?}: {:?}", msg.chat, &txt[offset..]);
-                    let mut req = msg.text_reply(ROLL_ERR_MSG);
-                    req.parse_mode(ParseMode::Markdown);
-                    api.send(req).await
-                }
-            }
-        }
+pub(crate) async fn handle(update: Update) -> Result {
+    let msg = match update {
+        Update::NewMessage(m) if !m.outgoing() && !m.text().is_empty() => m,
         _ => {
-            warn!("unknown command on {:?}: {:?}", msg.chat, txt);
-            api.send(msg.text_reply(UNKNOWN_MSG)).await
+            trace!("ignoring: {:?}", update);
+            return Ok(());
         }
     };
 
-    info!("send result: {:?}", res);
+    let cmd = Command::from(msg.text());
+    match msg.sender() {
+        Some(user) if user.id() != msg.chat().id() => msg.reply(cmd).await?,
+        _ => msg.respond(cmd).await?,
+    };
+
     Ok(())
 }
 
@@ -100,41 +73,72 @@ pub async fn handle(api: &Api, update: Update) -> Result<(), Error> {
 enum Command {
     Start,
     Help,
-    Roll(usize),
+    Roll(Roll),
     Unknown,
 }
 
-impl Command {
-    fn resolve(txt: &str, entities: &[MessageEntity]) -> Option<Command> {
-        use telegram_bot::MessageEntityKind::BotCommand;
+impl From<&str> for Command {
+    fn from(value: &str) -> Self {
+        match parse_command(value) {
+            Ok((_, cmd)) => cmd,
+            Err(e) => {
+                warn!("malformed command received: {}", e);
+                Command::Unknown
+            }
+        }
+    }
+}
+
+impl Into<InputMessage> for Command {
+    fn into(self) -> InputMessage {
         use Command::*;
-
-        let ent = entities.first()?;
-
-        if ent.kind != BotCommand || ent.offset != 0 {
-            return None;
-        }
-
-        let slash = Self::isolate_slash(&txt[ent.offset as usize..(ent.offset + ent.length) as usize])?;
-
-        let cmd = match slash.to_lowercase().as_str() {
-            "/start" => Start,
-            "/help" => Help,
-            "/r" | "/roll" => Roll((ent.offset + ent.length) as usize),
-            _ => Unknown,
-        };
-
-        Some(cmd)
-    }
-
-    fn isolate_slash(txt: &str) -> Option<&str> {
-        use crate::BOT_NAME;
-        let parts: Vec<&str> = txt.split('@').collect();
-
-        match parts.len() {
-            1 => Some(parts[0]),
-            2 if parts[1] == BOT_NAME => Some(parts[0]),
-            _ => None,
+        match self {
+            Start => InputMessage::markdown(START_MSG),
+            Help => InputMessage::markdown(HELP_MSG),
+            Roll(r) => {
+                let result = format!("{} = {}", r.value(), r);
+                info!("roll: {}", result);
+                InputMessage::markdown(result)
+            }
+            Unknown => InputMessage::markdown(UNKNOWN_MSG),
         }
     }
+}
+
+fn parse_command(input: &str) -> IResult<&str, Command> {
+    preceded(
+        opt(char('/')),
+        alt((
+            parse_start,
+            parse_help,
+            parse_roll,
+        )),
+    )(input)
+}
+
+fn parse_start(input: &str) -> IResult<&str, Command> {
+    let (input, _) = tuple((
+        tag_no_case("start"),
+        alt((multispace1, eof)),
+        rest,
+    ))(input)?;
+    Ok((input, Command::Start))
+}
+
+fn parse_help(input: &str) -> IResult<&str, Command> {
+    let (input, _) = tuple((
+        tag_no_case("help"),
+        alt((multispace1, eof)),
+        rest,
+    ))(input)?;
+    Ok((input, Command::Help))
+}
+
+fn parse_roll(input: &str) -> IResult<&str, Command> {
+    let (input, _) = opt(alt((
+        tag_no_case("roll"),
+        tag_no_case("r"),
+    )))(input)?;
+    let roll = rolls::parse(input)?;
+    Ok(("", Command::Roll(roll)))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,76 @@
-extern crate tokio;
-extern crate futures;
-extern crate telegram_bot;
-extern crate rand;
+use std::env;
+use std::error::Error;
+use std::pin::pin;
+
+use grammers_client::{Client, Config, InitParams};
+use grammers_session::Session;
+use log::{error, trace};
+use tokio::{select, task};
+
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 mod handler;
 mod rolls;
 
-use std::env;
-use futures::StreamExt;
-use telegram_bot::*;
-
-const BOT_NAME: &str = "DiceGoblinTestBot";
+const API_ID_VAR: &str = "DICE_GOBLIN_API_ID";
+const API_HASH_VAR: &str = "DICE_GOBLIN_API_HASH";
 const TOKEN_VAR: &str = "DICE_GOBLIN_TOKEN";
+const SESSION_VAR: &str = "DICE_GOBLIN_SESSION";
+
+type Result = std::result::Result<(), Box<dyn Error>>;
 
 #[tokio::main]
-async fn main() {
-    let token = env::var(TOKEN_VAR)
-        .unwrap_or_else(|_| panic!("{} not set", TOKEN_VAR));
-    let api = Api::new(token);
+async fn main() -> Result {
+    simple_logger::init_with_env()?;
 
-    stderrlog::new()
-        .module(module_path!())
-        .timestamp(stderrlog::Timestamp::Nanosecond)
-        .verbosity(log::LevelFilter::Info as usize)
-        .init()
-        .unwrap();
+    let session_file = env::var(SESSION_VAR)?;
+    let api_id = env::var(API_ID_VAR)?.parse()?;
+    let api_hash = env::var(API_HASH_VAR)?;
+    let token = env::var(TOKEN_VAR)?;
 
-    let mut stream = api.stream();
-    while let Some(update) = stream.next().await {
-        if let Err(e) = update {
-            eprintln!("Error from the stream: {}", e);
-            continue;
+    trace!("connecting to Telegram...");
+    let client = Client::connect(
+        Config {
+            api_id,
+            api_hash,
+            session: Session::load_file_or_create(&session_file)?,
+            params: InitParams {
+                catch_up: true,
+                ..Default::default()
+            },
         }
+    ).await?;
+    trace!("connected");
 
-        if let Err(e) = handler::handle(&api, update.unwrap()).await {
-            eprintln!("Error sending request: {}", e);
+    if !client.is_authorized().await? {
+        trace!("Signing in...");
+        client.bot_sign_in(&token).await?;
+        if let Err(e) =  client.session().save_to_file(&session_file) {
+            client.sign_out().await?;
+            return Err(e.into());
         }
+        trace!("Signed in!")
     }
+
+    let mut exit = pin!(tokio::signal::ctrl_c());
+    loop {
+        let update = select! {
+            _ = &mut exit => None,
+            upd = client.next_update() => upd?,
+        };
+        match update {
+            None => break,
+            Some(upd) => task::spawn(async move {
+                match handler::handle(upd).await {
+                    Ok(_) => {},
+                    Err(e) => error!("Error handling update: {}", e)
+                }
+            }),
+        };
+    }
+
+    trace!("Exiting...");
+    client.session().save_to_file(&session_file)?;
+    Ok(())
 }
